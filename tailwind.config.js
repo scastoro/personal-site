ebca33fd3ad2269f3cd52397867836aa03094c20
Sal Castoro <salcastoro@gmail.com>
2023-07-23 09:29:54 -0500

    Add colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,20 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      white: '#F8F8F8',
+      'light-blue': '#45DDFB',
+      black: '#0D0D0D',
+      'light-black': '#151517',
+      'light-grey': '#E3E3E3',
+      'dark-grey': '#C4C4C4',
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
   plugins: [],
-}
+};
