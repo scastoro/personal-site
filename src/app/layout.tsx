237ab5ce33b0d1a6a085d6048a934e8e0b68303e
Sal Castoro <salcastoro@gmail.com>
2023-07-23 18:30:44 -0500

    Add tailwind config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,33 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter, Oswald, Poppins, Source_Code_Pro } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const oswald = Oswald({ subsets: ['latin'], display: 'swap', variable: '--font-oswald' });
+const sourceCodePro = Source_Code_Pro({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-code',
+});
+const poppins = Poppins({
+  weight: ['200', '400', '600', '900'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-poppins',
+});
 
 export const metadata: Metadata = {
-  title: 'Sal Castoro | Web Development | UI Design | UI Development',
-  description: 'Sal Castoro fullstack web developer',
-}
+  title: 'Sal Castoro | Web Development | UI Design | UI Development | Freelance',
+  description: 'Sal Castoro fullstack freelance web developer',
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en' className='overflow-x-hidden scroll-smooth'>
+      <body
+        className={`${oswald.variable} ${sourceCodePro.variable} ${poppins.variable} ${poppins.className} relative overflow-x-hidden`}
+      >
+        {children}
+      </body>
     </html>
-  )
+  );
 }
